1ed5c443d46c6250aec31d72d93db42a3504a36d
JoaoVitorPaludo <joaovpaludo@gmail.com>
2025-12-22 15:16:09 -0300

    feat: Adicionando padding ao container da seção hero

--- src/modules/home/pages/hero/styles.ts
+++ src/modules/home/pages/hero/styles.ts
@@ -38,6 +38,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 0 1rem;
 
   @media (min-width: 640px) {
     max-width: 640px;
